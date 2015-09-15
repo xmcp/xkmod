da4be2be6a67bb71a60d63dda2f18f45a37d7629
xmcp <f@bi.us>
2015-09-15 19:53:03 +0800

    beta

--- deploy/deploy.py
+++ deploy/deploy.py
@@ -2,16 +2,17 @@
 import base64
 
 with open('../src/xkmain.html','r',encoding='utf-8') as f:
-    main_content='\n'.join((x.lstrip() for x in f.read().split('\n')))
+    main_content='\n'.join((x.lstrip() for x in f.read().split('\n'))).replace('\n\n','\n')
 
 with open('../src/xkmain.js','r',encoding='utf-8') as f:
-    main_script='\n'.join((x.lstrip() for x in f.read().split('\n')))
+    main_script='\n'.join((x.lstrip() for x in f.read().split('\n'))).replace('\n\n','\n')
 
 with open('../src/xkboot.js','r',encoding='utf-8') as f:
-    boot_content='\n'.join((x.lstrip() for x in f.read().split('\n')))
+    boot_content='\n'.join((x.lstrip() for x in f.read().split('\n'))).replace('\n\n','\n')
 
 boot_content=boot_content.replace('/*base_body*/',base64.b64encode(main_content.encode('utf-8')).decode())\
                          .replace('/*base_script*/',base64.b64encode(main_script.encode('utf-8')).decode())
 
 with open('xkboot.min.js','w') as f:
+    f.write('// THIS CODE IS AUTO GENERATED. DO NOT CHANGE HERE.\n')
     f.write(boot_content)
--- src/xkboot.js
+++ src/xkboot.js
@@ -1,37 +1,39 @@
+// https://github.com/xmcp/xkmod
+
 if(document.domain!=='127.0.0.1' && document.domain!=='cms.rdfz.cn') {
-    document.write('Please run the script in "cms.rdfz.cn" domain');
-    throw 1;
+  document.write('Please run the script in "cms.rdfz.cn" domain');
+  throw 1;
 }
 
-xkhead='<title>xkmod</title>\n<meta charset="utf-8">';
-xkbody=decodeURIComponent(escape(window.atob('/*base_body*/')));
-xkscript='/*base_script*/';
-htmlroot=document.getElementsByTagName('html')[0];
-
 (function() {
-    var nw=window.open('/test'); //check popup blocker
-    if(nw===undefined) {
-        document.write('Please allow the pop-up');
-        throw 1;
-    } else { //well done
-        nw.close();
+  var xkhead='<title>xkmod</title>\n<meta charset="utf-8">';
+  var xkbody=decodeURIComponent(escape(window.atob('/*base_body*/'))); //utf-8 magic
+  var xkscript='/*base_script*/';
+  var htmlroot=document.getElementsByTagName('html')[0];
 
-        if(!document.head)
-            htmlroot.appendChild(document.createElement('head'));
-        if(!document.body)
-            htmlroot.appendChild(document.createElement('body'));
+  var nw=window.open('/test'); //check popup blocker
+  if(nw===undefined) {
+    document.write('Please allow the pop-up');
+    throw 1;
+  } else { //well done
+    nw.close();
 
-        document.head.innerHTML=xkhead;
-        document.body.innerHTML=xkbody;
+    if(!document.head)
+      htmlroot.appendChild(document.createElement('head'));
+    if(!document.body)
+      htmlroot.appendChild(document.createElement('body'));
 
-        var jquery=document.createElement('script');
-        jquery.src='http://libs.useso.com/js/jquery/2.1.1/jquery.min.js';
-        document.head.appendChild(jquery);
+    document.head.innerHTML=xkhead;
+    document.body.innerHTML=xkbody;
 
-        setTimeout(function() {
-            var myscript=document.createElement('script');
-            myscript.src='data:text/javascript;charset=utf-8;base64,'+xkscript;
-            document.head.appendChild(myscript);
-        },250);
-    }
+    var jquery=document.createElement('script');
+    jquery.src='http://libs.useso.com/js/jquery/2.1.1/jquery.min.js';
+    document.head.appendChild(jquery);
+
+    setTimeout(function() {
+      var myscript=document.createElement('script');
+      myscript.src='data:text/javascript;charset=utf-8;base64,'+xkscript;
+      document.head.appendChild(myscript);
+    },250);
+  }
 })();
